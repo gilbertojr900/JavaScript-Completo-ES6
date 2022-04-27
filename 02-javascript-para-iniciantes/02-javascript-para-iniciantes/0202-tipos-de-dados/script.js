1bb259e559c984f37d13a45b3cb1c936b2cb087f
Gilberto Menegon Júnior <99845776+gilbertojr900@users.noreply.github.com>
2022-04-27 15:50:44 -0300

    qwe

--- 02-javascript-para-iniciantes/02-javascript-para-iniciantes/0202-tipos-de-dados/script.js
+++ 02-javascript-para-iniciantes/02-javascript-para-iniciantes/0202-tipos-de-dados/script.js
@@ -1,15 +1,15 @@
 // Declare uma variável contendo uma string
-var nome = 'André';
+var nome = 'Gilberto';
 
 // Declare uma variável contendo um número dentro de uma string
-var ano = '2018';
+var ano = '2022';
 
 // Declare uma variável com a sua idade
-var idade = 28;
+var idade = 31;
 
 // Declare duas variáveis, uma com seu nome
 // e outra com seu sobrenome e some as mesmas
-var sobrenome = 'Rafael'
+var sobrenome = 'Menegon'
 var nomeCompleto = `${nome} ${sobrenome}`;
 
 // Coloque a seguinte frase em uma variável: It's time
